0c25737e99eed072a72bd4493501129de8d8c48f
CeciliaWahome <wahomececilia@gmail.com>
2017-03-10 15:31:55 +0300

    add function

--- done.py
+++ done.py
@@ -1,10 +1,18 @@
-def done_list(studied_list):
-	studied_list = list(input("Enter the skills I've studied: "))
-	for item in study_list:
-		for j in studied_list:
-			if item == j:
-				study_list.remove(item)
-				studied_list.append(j)
-				print(studied_list)
-			else:
-				print("item entered is not in our to do list")
+
+def done_list(studied_list, study_list):
+    studied_list = []
+    not_studied = []
+    
+    for item in study_list:
+        if item not in study_list:
+            print("Error in input")
+            return studied_list, study_list
+        else:
+            study_list.remove(item)
+            studied_list.append(item)
+    return studied_list, study_list
+
+studied_list = input("Enter the skills I've studied: ")
+studied_list = studied_list.split(",")
+
+print(done_list(studied_list, study_list))
